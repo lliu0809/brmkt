54fecb2e58ead20716bc4f4f40526bcc8e6d868b
JasonLai42 <jasonlaiusa@yahoo.com>
2020-10-16 12:54:47 -0700

    redo repo

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM gcr.io/distroless/nodejs:12
-
-COPY server/bin /bin
-
-EXPOSE 3000
-
-WORKDIR /bin
-CMD ["server/src/server.js"]
--- web/src/style/fonts.ts
+++
@@ -1,5 +0,0 @@
-export const Fonts = {
-  sansHeader: "'SF Display', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-  sansBody: "'SF Text', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-  mono: "'SF Mono', monospace",
-}
