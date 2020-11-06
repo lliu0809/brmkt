83d62a07de399795bce02fe434f7e70bd06494b1
JasonLai42 <jasonlaiusa@yahoo.com>
2020-11-06 12:09:21 -0800

    auction tab fetched data

--- brmkt_src/web/src/view/page/AuctionsPage.tsx
+++ brmkt_src/web/src/view/page/AuctionsPage.tsx
@@ -1,22 +1,40 @@
+import { useQuery } from '@apollo/client'
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
+import { FetchAuctions } from '../../graphql/query.gen'
+import { Spacer } from '../../style/spacer'
 import { AppRouteParams } from '../nav/route'
+import { fetchAuctions } from '../page/fetchAuctions'
 import { Page } from './Page'
 
 interface AuctionsPageProps extends RouteComponentProps, AppRouteParams {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function AuctionsPage(props: AuctionsPageProps) {
-  fetch('/auctions')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => {
-      console.error(err)
-    })
+  const { loading, data } = useQuery<FetchAuctions>(fetchAuctions)
 
-  return (
-    <Page>
-      <div>hello world!</div>
-    </Page>
-  )
+  if (loading || data == null) {
+    return <div>loading...</div>
+  }
+
+  else if (!data || data.auctions.length === 0) {
+    return <div>no auctions</div>
+  }
+  else  {
+    return (
+      <Page>
+        <div className="mw6">
+          <Spacer $h4 />
+          {data.auctions
+
+            .map((auction, i) => (
+              <div key={i} className="pa3 br2 mb2 bg-black-10 flex items-center">
+                <Spacer $w4 />
+                {auction.title} · {auction.price}
+              </div>
+            ))}
+        </div>
+      </Page>
+    )
+  }
 }
