64600096712469686a9f18acec4a5ce6484dab58
ylyyyyu <56143170+ylyyyyu@users.noreply.github.com>
2020-11-11 15:14:53 +0800

    added load test
    createUser, login, fetchAuctions, homepage
    run brew `brew install k6` and `brew install tsung` for running load tests
    k6: k6 run script.js
    tsung: tsung -f test.xml -k start

--- brmkt_src/script.js
+++ brmkt_src/script.js
@@ -1,8 +1,8 @@
 import http from 'k6/http'
- // import { sleep } from 'k6'
- import { Counter, Rate } from 'k6/metrics'
+import { sleep } from 'k6'
+import { Counter, Rate } from 'k6/metrics'
 
- export const options = {
+/*export const options = {
    scenarios: {
      example_scenario: {
        // name of the executor to use
@@ -19,47 +19,76 @@ import http from 'k6/http'
        ],
      },
    },
- }
+ }*/
 
- export default function () {
-   // recordRates(
-   const resp = http.post(
-     'http://localhost:3000/graphql',
-     '{"operationName":"AnswerSurveyQuestion","variables":{"input":{"answer":"🤗","questionId":1}},"query":"mutation AnswerSurveyQuestion($input: SurveyInput!) {\\n  answerSurvey(input: $input)\\n}\\n"}',
-     {
-       headers: {
-         'Content-Type': 'application/json',
-       },
-     }
-   )
-   // )
-   sleep(1)
-   http.get('http://localhost:3000')
- }
+export default function () {
+  //load test homepage
+  http.get('http://localhost:3000')
+  sleep(1)
 
- const count200 = new Counter('status_code_2xx')
- const count300 = new Counter('status_code_3xx')
- const count400 = new Counter('status_code_4xx')
- const count500 = new Counter('status_code_5xx')
+  //load test createUser
+  const resp = http.post(
+    'http://localhost:3000/auth/createUser',
+    '{"email": "email1@gmail.com","name": "my friend","userType": "USER","address": "address","cardNumber": "1234","password": "password","id": 19,"timeCreated": {},"timeUpdated": {},"userType":"USER"}',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+  sleep(1)
 
- const rate200 = new Rate('rate_status_code_2xx')
- const rate300 = new Rate('rate_status_code_3xx')
- const rate400 = new Rate('rate_status_code_4xx')
- const rate500 = new Rate('rate_status_code_5xx')
+  //load test fetchAuctions
+  const resp1 = http.post(
+    'http://localhost:3000/graphql',
+    '{"operationName":"FetchAuctions","variables":{},"query":"query FetchAuctions {\\n  auctions {\\n    ...AuctionTopBid\\n    __typename\\n  }\\n}\\n\\nfragment Auction on Auction {\\n  id\\n  title\\n  price\\n  description\\n  prodType\\n  seller\\n  currentHighest\\n  auctionTime\\n  status\\n  __typename\\n}\\n\\nfragment AuctionTopBid on AuctionTopBid {\\n  topBid\\n  auction {\\n    ...Auction\\n    __typename\\n  }\\n  __typename\\n}\\n"}',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
 
- function recordRates(res) {
-   if (res.status >= 200 && res.status < 300) {
-     count200.add(1)
-     rate200.add(1)
-   } else if (res.status >= 300 && res.status < 400) {
-     console.log(res.body)
-     count300.add(1)
-     rate300.add(1)
-   } else if (res.status >= 400 && res.status < 500) {
-     count400.add(1)
-     rate400.add(1)
-   } else if (res.status >= 500 && res.status < 600) {
-     count500.add(1)
-     rate500.add(1)
-   }
- }
+  sleep(1)
+
+  //load test login
+  const resp2 = http.post(
+    'http://localhost:3000/auth/login',
+    '{"email": "email1@gmail.com","password": "password"}',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+  sleep(1)
+
+  //load test logout
+}
+
+const count200 = new Counter('status_code_2xx')
+const count300 = new Counter('status_code_3xx')
+const count400 = new Counter('status_code_4xx')
+const count500 = new Counter('status_code_5xx')
+
+const rate200 = new Rate('rate_status_code_2xx')
+const rate300 = new Rate('rate_status_code_3xx')
+const rate400 = new Rate('rate_status_code_4xx')
+const rate500 = new Rate('rate_status_code_5xx')
+
+function recordRates(res) {
+  if (res.status >= 200 && res.status < 300) {
+    count200.add(1)
+    rate200.add(1)
+  } else if (res.status >= 300 && res.status < 400) {
+    console.log(res.body)
+    count300.add(1)
+    rate300.add(1)
+  } else if (res.status >= 400 && res.status < 500) {
+    count400.add(1)
+    rate400.add(1)
+  } else if (res.status >= 500 && res.status < 600) {
+    count500.add(1)
+    rate500.add(1)
+  }
+}
