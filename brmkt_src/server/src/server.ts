a41ea5fa535bd4c2a4fe4b09b42358f7bc30b857
ylyyyyu <56143170+ylyyyyu@users.noreply.github.com>
2020-11-13 05:02:33 +0800

    updated honeycomb key

--- brmkt_src/server/src/server.ts
+++ brmkt_src/server/src/server.ts
@@ -1,5 +1,5 @@
 require('honeycomb-beeline')({
-  writeKey: process.env.HONEYCOMB_KEY || 'd29d5f5ec24178320dae437383480737',
+  writeKey: process.env.HONEYCOMB_KEY || 'b93712f14497199bc5a3918b312c59b5',
   dataset: process.env.APP_NAME || 'bespin',
   serviceName: process.env.APPSERVER_TAG || 'local',
   enabledInstrumentations: ['express', 'mysql2', 'react-dom/server'],
