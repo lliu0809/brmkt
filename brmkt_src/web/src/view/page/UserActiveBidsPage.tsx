0d6d307f83b05a50285946cac2cffb421322ebb7
ylyyyyu <56143170+ylyyyyu@users.noreply.github.com>
2020-11-21 04:59:14 +0800

    Revert "Revert "Merge branch 'dev' of https://github.com/scalableinternetservices/brmkt into dev""
    This reverts commit d94b56e2a42777c981c61895509ad40eb49a53ec.

--- brmkt_src/web/src/view/page/UserActiveBidsPage.tsx
+++ brmkt_src/web/src/view/page/UserActiveBidsPage.tsx
@@ -6,21 +6,26 @@ import { FetchMyActiveBids, FetchMyActiveBidsVariables } from '../../graphql/que
 import { H1, H3 } from '../../style/header';
 import { Spacer } from '../../style/spacer';
 import { style } from '../../style/styled';
+import { UserContext } from '../auth/user';
 import { AppRouteParams } from '../nav/route';
+import { LogInPage } from './LogInPage';
+import { Page } from './Page';
 import { fetchMyActiveBids } from './queries/fetchAuctions';
 
 interface UserActiveBidsPageProps extends RouteComponentProps, AppRouteParams {}
 
 export function UserActiveBidsPage(props: UserActiveBidsPageProps) {
-  // const user = React.useContext(UserContext)
+  const user = React.useContext(UserContext)
 
-  return <MyActiveBids bidderId={Number(2)}/>
-
-  // if(!user.user) {
-  //   // NEED REDIRECT TO LOGIN
-  // } else {
-  //   return <MyListings sellerId={Number(user.user.id)}/>
-  // }
+  if(!user.user) {
+    return <LogInPage/>
+  } else {
+    return (
+      <Page>
+        <MyActiveBids bidderId={user.user.id}/>
+      </Page>
+    )
+  }
 }
 
 function MyActiveBids({ bidderId }: { bidderId: number }) {
@@ -63,7 +68,6 @@ function MyActiveBids({ bidderId }: { bidderId: number }) {
                 </Description>
               </Product>
                 {myActiveBid.auctionTopBid.auction.id} · {myActiveBid.auctionTopBid.auction.title} · {myActiveBid.auctionTopBid.topBid}{' '}
-                <img src={'/app/assets/auction/NEW chair.png'} />
               <Spacer $w4 />
             </div>
           ))}
